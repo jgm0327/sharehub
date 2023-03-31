fc58f7f1460b5aa99b669cd16aea74a43215e72d
jgm0327 <gmjang6349@gmail.com>
2023-03-31 14:04:53 +0900

    프로젝트 초기 설정
    web, security, mysqlDriver, JPA, lombok 를 가져오고 application.yml 파일로 db 설정함

---
+++ src/main/java/com/example/sharehub/SharehubApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharehubApplication.class, args);
+    }
+
+}
