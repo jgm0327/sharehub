d85a63d68c9e564630241243f163454a3def25f0
jgm0327 <gmjang6349@gmail.com>
2023-04-01 18:17:30 +0900

    @JsonProperty 추가
    클라이언트가 요청 시 전달하는 파라미터와 매핑을 위해서 동일한 이름으로 받도록 설정

---
+++ src/main/java/com/example/sharehub/authentication/dto/AuthenticationDTO.java
@@ -0,0 +1,57 @@
+package com.example.sharehub.authentication.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+@Data
+public class AuthenticationDTO implements UserDetails {
+
+    @JsonProperty("userId")
+    private String username;
+    @JsonProperty("userPassword")
+    private String password;
+    @JsonIgnore
+    private String role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
